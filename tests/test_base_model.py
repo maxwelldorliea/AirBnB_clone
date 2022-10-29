30ca247ddcb2b447b6986617ecbfee65606012ef
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-29 10:10:03 +0000

    Setup base model unittest

---
+++ tests/test_base_model.py
@@ -0,0 +1,10 @@
+import unittest
+
+"""Base Model Unittest Module."""
+
+class TestBaseModel(unittest.TestCase):
+    """Represent the test model for Base Model."""
+
+    def test_id(self):
+        """Test if id is unique."""
+        pass
