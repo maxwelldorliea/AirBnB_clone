6885f59f348aff5f741628dd958128621c59130e
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-29 11:45:10 +0000

    Added 3 test cases in base model unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,26 @@
 import unittest
+from models import base_model
 
 """Base Model Unittest Module."""
 
 class TestBaseModel(unittest.TestCase):
     """Represent the test model for Base Model."""
 
+    def setUp(self):
+        self.b1 = base_model.BaseModel()
+        self.b2 = base_model.BaseModel()
+        self.objs_dict = self.b1.to_dict()
+        self.created_at = self.objs_dict['created_at']
+        self.updated_at = self.objs_dict['updated_at']
+
     def test_id(self):
         """Test if id is unique."""
-        pass
+        self.assertNotEqual(self.b1.id, self.b2.id)
+
+    def test_dict_created_at(self):
+        """Test if created_at is a str."""
+        self.assertIsInstance(self.created_at, str)
+    
+    def test_dict_updated_at(self):
+        """Test if updated_at is a str."""
+        self.assertIsInstance(self.updated_at, str)
