860f23181120ec619055779f6fd45c31ea6ae5a7
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-31 00:12:35 +0000

    Added unittest for review model

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""This is the Review Module."""
+from models import BaseModel
+from models.review import Review
+import unittest
+
+
+
+class TestReview(unittest.TestCase):
+    """Implement unittest for Review."""
+
+    def setUp(self) -> None:
+        """Set up Review unittest."""
+        self.r1 = Review()
+
+    def test_text(self):
+        """Test if  Review has attribute text."""
+        self.assertTrue(hasattr(self.r1, 'text'))
+
+    def test_text_empty(self):
+        """Test attribute text is empty."""
+        self.assertEqual(self.r1.text, '')
+    
+    def test_user_id(self):
+        """Test if  Review has attribute user_id."""
+        self.assertTrue(hasattr(self.r1, 'user_id'))
+
+    def test_user_id_empty(self):
+        """Test attribute user_id is empty."""
+        self.assertEqual(self.r1.user_id, '')
+    
+    def test_place_id(self):
+        """Test if  Review has attribute place_id."""
+        self.assertTrue(hasattr(self.r1, 'place_id'))
+
+    def test_place_id_empty(self):
+        """Test attribute place_id is empty."""
+        self.assertEqual(self.r1.place_id, '')
+
+
+
+    def test_issubclass_base_model(self):
+        """Test if Review is subclass of BaseModel."""
+        self.assertIsInstance(self.r1, BaseModel)
